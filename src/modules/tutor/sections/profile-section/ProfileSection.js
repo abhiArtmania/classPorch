2287fd8750355ab204a3f2995fefae985693d19d
VienNguyen <luongvien13t2@gmail.com>
2018-04-01 12:51:21 +0700

    map data to About Me of tutor profile

--- src/modules/tutor/sections/profile-section/ProfileSection.js
+++ src/modules/tutor/sections/profile-section/ProfileSection.js
@@ -9,25 +9,15 @@ const ProfileSection = props => {
     profile,
     educationalAttributes,
     userId,
-    role,
-    authToken,
     presentProfileId,
     mode,
     onChangeEducation,
     toggleProfileMode,
-    reviews
   } = props;
 
   return(
     <div style={{width:'100%',paddingBottom:'5em'}} >
-      <AboutSegment
-        profile={profile} 
-        mode={props.mode}
-        presentProfileId={presentProfileId}
-        userId={userId}
-        toggleProfileMode={props.toggleProfileMode}
-        onChangeUserInfo={props.onChangeUserInfo}
-      />
+      <AboutSegment data={profile} />
       <div className="ui clearing divider"></div>  
       <ReviewsSection data={profile.reviews} />
       <EducationSegment
--- src/modules/tutor/sections/profile-section/segments/AboutSegment.js
+++ src/modules/tutor/sections/profile-section/segments/AboutSegment.js
@@ -1,149 +1,51 @@
 import React, {Component} from 'react'
-import {Grid, Button, Rating} from "semantic-ui-react";
-import PropTypes from 'prop-types';
+import { Grid } from "semantic-ui-react";
+import { object } from 'prop-types';
 import '../../../styles.css'
 import Truncate from 'react-truncate';
 
 
 class AboutSegment extends Component {
-    constructor(...args) {
-        super(...args);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            count: 5,
-            defaultCount: 5,
-            expanded: false,
-            truncated: false
-        };
-        this.data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 6, 4, 3, 2, 1, 5];
-        this.handleTruncate = this.handleTruncate.bind(this);
-        this.toggleLines = this.toggleLines.bind(this);
-    }
-    handleCount() {
-        let count = this.state.defaultCount;
-        count = count + this.state.count;
-        this.setState({ count });
-      }
-    handleTruncate(truncated) {
-        if (this.state.truncated !== truncated) {
-            this.setState({
-                truncated
-            });
-        }
-    }
-
-    toggleLines(event) {
-        event.preventDefault();
-
-        this.setState({
-            expanded: !this.state.expanded
-        });
-    }
-
-    onChangeField = (field, e, {value}) => {
-        this.props.onChangeUserInfo(field, value)
+    this.state = {
+      expanded: false,
     };
+  }
 
-    onClickEdit = () => {
-        this.props.toggleProfileMode('edit')
-    };
+  toggleLines = event => {
+    event.preventDefault();
+    this.setState(prevState => {
+      return { expanded: !prevState.expanded };
+    });
+  }
 
-
-    onFocusChange = (event) => {
-        if (event.type === 'focus') {
-            event.target.type = 'date';
-            event.target.click()
-        } else {
-            event.target.type = 'text'
-        }
-    };
-
-    render() {
-        const {profile} = this.props;
-
-        const fullName = profile['full-name'];
-        const birthdayDate = profile['birthday date'];
-        const gender = profile['gender'];
-        const textv = <p >Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-             It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>;
-         const {
-           
-            more,
-            less,
-            lines
-        } = this.props;
-
-        const {
-            expanded,
-            truncated
-        } = this.state;
-        const count = this.state.count;
-        return (
-            <Grid padded relaxed style={{width: '100%', paddingTop: 30}}>
-            <Grid.Column width={16} >
-                        
-                        <h2>About Me</h2>
-                        
-                        <div>
-                            <Truncate lines={!expanded && lines} ellipsis={(<span>... <a href='#' onClick={this.toggleLines}>{more}</a></span>
-                            )}  onTruncate={this.handleTruncate} >
-                                {textv}
-                            </Truncate>
-                            {!truncated && expanded && (<span> <a href='#' onClick={this.toggleLines}>{less}</a></span>)}
-                        </div>
-                        </Grid.Column>
-                
-                
-            </Grid>
-        )
-    }
+  render() {
+    const { expanded } = this.state;
+    return (
+      <Grid padded relaxed style={{width: '100%', paddingTop: 30}}>
+        <h2>About Me</h2>
+        <div>
+          <Truncate
+            lines={!expanded && 3}
+            ellipsis={(<span>... <a onClick={this.toggleLines}>Read more</a></span>)}
+          >
+            {this.props.data.bio}
+          </Truncate>
+          {expanded && (<span> <a onClick={this.toggleLines}>Show less</a></span>)}
+        </div>
+      </Grid>
+    )
+  }
 }
+
 AboutSegment.defaultProps = {
-    lines: 3,
-    more: 'Read more',
-    less: 'Show less'
+  data: {},
 };
 
 AboutSegment.propTypes = {
-    text: PropTypes.node,
-    lines: PropTypes.number
+  data: object,
 };
-/*<Grid.Row stretched columns={1} centered>
-                    <Grid.Column width={12} textAlign='left'>
-                        <div className='sub-heading'> ABOUT</div>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row stretched columns={2} centered>
-                    <Grid.Column width={1}>
-                        <Icon name='student' size='large' color='grey'/>
-                    </Grid.Column>
-                    <Grid.Column width={3} textAlign='left'>
-                        <div className='profile-text'> {fullName} </div>
-                    </Grid.Column>
-                    <Grid.Column width={8} textAlign='left'/>
-                </Grid.Row>
-                <Grid.Row stretched columns={2} centered>
-                    <Grid.Column width={1}>
-                        <Icon name='calendar' size='large' color='grey'/>
-                    </Grid.Column>
-                    <Grid.Column width={3} textAlign='left'>
-                        <div className='profile-text'> {birthdayDate} </div>
-                    </Grid.Column>
-                    <Grid.Column width={8} textAlign='left'/>
-                </Grid.Row>
-                <Grid.Row stretched columns={1} centered>
-                    <Grid.Column width={1} textAlign='left'>
-                        <div style={{fontWeight: 'bold'}}> Gender</div>
-                    </Grid.Column>
-                    <Grid.Column width={3} textAlign='left'>
-                        <div className='profile-text' style={{textTransform: "capitalize"}}> {gender} </div>
-                    </Grid.Column>
-                    <Grid.Column width={8} textAlign='left'/>
-                </Grid.Row>
-*/
+
 export default AboutSegment;
