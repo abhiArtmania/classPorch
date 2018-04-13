45f9ee0537b9faafad716d0126be89fca44e2dbc
Hassaan Khan <hassaankhan0096@gmail.com>
2018-04-14 01:08:35 +0500

    Submit Ticket and Faq Changes

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,7 +5,7 @@ import SearchReducer from './SearchReducer';
 import ProfileReducer from './ProfileReducer';
 import ChatReducer from './ChatReducer';
 import MessageReducer from './MessageReducer';
-import tutors from './tutors';
+import tutors from './tutor';
 
 export default {
     auth: AuthReducer,
